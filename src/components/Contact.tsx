c8d5fa359fa12d1cbffac82ae6a67c5d62bff753
Your Name <freek.herregods@student.howest.be>
2024-11-16 11:08:31 +0100

    contactpagina gemaakt

---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact: React.FC = () => {
+  return (
+    <div className='app-container'>
+      <h1>Contact Page</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+};
+
+export default Contact;
